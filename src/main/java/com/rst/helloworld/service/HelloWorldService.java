0182c132300a25c274387f9b7e07e87d48793cd9
devopsloki <98764796+devopsloki@users.noreply.github.com>
2023-02-13 12:35:37 +0530

    FineOS related

--- src/main/java/com/rst/helloworld/service/HelloWorldService.java
+++ src/main/java/com/rst/helloworld/service/HelloWorldService.java
@@ -14,7 +14,7 @@ public class HelloWorldService {
 
 		logger.debug("getDesc() is executed!");
 
-		return "Maven + Spring MVC + Jenkins + Docker Example";
+		return "Lokesh Docker Demo with image and container Example";
 
 	}
 
@@ -23,7 +23,7 @@ public class HelloWorldService {
 		logger.debug("getTitle() is executed! $name : {}", name);
 
 		if(StringUtils.isEmpty(name)){
-			return "Hello Docker";
+			return "Hello Team";
 		}else{
 			return "Hello " + name;
 		}
